6b6d69bb9abffaa66e2876888926ac1fcc56ee75
TheDivic <divicnikola@gmail.com>
2015-05-23 21:14:12 +0200

    Added Font Awesome to the project

--- Liftit/Converters/DateToWeekdayConverter.cs
+++ Liftit/Converters/DateToWeekdayConverter.cs
@@ -12,7 +12,7 @@ namespace Liftit.Converters
         public object Convert(object value, Type targetType, object parameter, string language)
         {
             DateTime date = (DateTime)value;
-            return String.Format("{0:dddd} at {0:Htt}", date);
+            return String.Format("{0:dddd}", date);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
